f36f09ac19598b2923f33ea8307b6defacb5bb06
Vsb19 <55589130+Vsb19@users.noreply.github.com>
2020-12-19 00:43:32 +0530

    Delete Nat.tf

--- Nat.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_nat_gateway" "Nat-Gw" {
-  allocation_id = "${aws_eip.eip.id}"  #Attachinh created Elastic Ip 
-  subnet_id     = "${aws_subnet.pub-sub-1.id}" #Subent id in which  Nat-Gw is placed
-}
